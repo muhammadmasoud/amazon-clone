c54d859338b286dce9a89e4566810e580092edec
mahmoudAshraf283 <mahmoudashraf28396@gmail.com>
2025-08-03 17:08:02 +0300

    handel connection between users and frontend

--- backend/users/api/views.py
+++ backend/users/api/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
-
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def signup(request):
--- backend/users/models.py
+++ backend/users/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    mobile = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    mobile = models.CharField(max_length=50, null=True, blank=True)
     profile_image = models.ImageField(upload_to='users_profile', null=True, blank=True)
     address = models.TextField(null=True, blank=True)
 
+    REQUIRED_FIELDS = ['email']  # Username & password are required by default
